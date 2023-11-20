a60f2e3476b03e61e2cbca57b33dc9ef8f2559b9
lina1505 <130980643+lina1505@users.noreply.github.com>
2023-11-20 09:57:41 +0100

    Delete view/deleteReclamation.php

--- view/deleteReclamation.php
+++
@@ -1,5 +0,0 @@
-<?php
-include 'C:/xampp/htdocs/ProjetWeb2A/Controller/réclamationC.php';
-$reclamationC = new reclamationC();
-$reclamationC->deleteReclamation($_GET["idReclamation"]);
-header('Location:listRéclamations.php');
