1dc736e9c791289c2c32d2495beeb260efa16ac6
lina1505 <lina.benabdallah@esprit.tn>
2023-12-06 17:07:27 +0100

    suppr

--- View/frontOffice/deleteReclamation.php
+++
@@ -1,5 +0,0 @@
-<?php
-include 'C:/xampp/htdocs/ProjetWeb2A/Controller/reclamationC.php';
-$reclamationC = new reclamationC();
-$reclamationC->deleteReclamation($_GET["id"]);
-header('Location:listReclamations.php');
